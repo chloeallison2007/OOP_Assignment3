8e78aef1610ad1a095eb1a0c81199028079dff30
chloeallison2007 <chloeallison2007@gmail.com>
2026-03-30 02:29:48 -0600

    Update Node.java

--- src/sait/sll/problemdomain/Node.java
+++ src/sait/sll/problemdomain/Node.java
@@ -1,5 +1,27 @@
 package sait.sll.problemdomain;
 
+//
+import java.io.Serializable;
+
+//Node class, has all the getters and setters
 public class Node {
+	
+	//attributes
+	private Object data;
+	private Node next;
+	
+	//constructor
+	public Node(Object data) {
+		this.data = data;
+		this.next = null;
+	}
+	
+	//Getters
+	public Object getData() { return data; } //
+	public Node getNext() { return next; } //
+	
+	//Setters
+	public void setData(Object data) { this.data = data; } //
+	public void setNext(Node next) { this.next = next; } //
 
 }
